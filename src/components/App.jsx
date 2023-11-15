85e93286d99a9399d990fd5ad6678373c5256a2a
SSSerg05 <ssserg05@gmail.com>
2023-11-15 13:22:32 +0200

    fix styles + cart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Component } from "react";
 
 import goodsArr from "../data/goods.json"
 import Goods from "./Goods";
+import Cart from "./Cart";
+import './App.css'
 
 export class App extends Component {
   
@@ -18,6 +20,7 @@ export class App extends Component {
     cartTemp[event.target.dataset.key] ? cartTemp[event.target.dataset.key]++ : cartTemp[event.target.dataset.key] = 1;
     // cartTemp++;
     this.setState({cart : cartTemp});
+    
     let count = this.state.count;
     count++;
     this.setState({'count' :  count})
@@ -25,21 +28,34 @@ export class App extends Component {
 
 
   render() {
-  
+    const {cart, count} = this.state;
+
+    // let showCart;
+    // if (count !== 0) {
+    //     showCart =  <Cart cart={ cart } goods={ goodsArr } />;
+    // }
+    // else {
+    //     showCart = 'Empty';
+    // }
+
     return (
       <div className="container">
         <h1>Cart</h1>
         <div className="goods-field" onClick={this.addToCart}>
           {
-            goodsArr.map(item => <Goods 
-              title={item.title} 
-              cost={item.cost} 
-              image={item.image} 
-              articul={item.articul} 
-              key={item.articul} 
-            />
+            goodsArr.map(item => 
+              <Goods 
+                title={item.title} 
+                cost={item.cost} 
+                image={item.image} 
+                articul={item.articul} 
+                key={item.articul} 
+              />
           )}
         </div>
+
+        { count !== 0 && <Cart cart={ cart } goods={ goodsArr } /> }
+
       </div>
     );  
   }
